782dbd747245ca5edd34c23bdda097c9b68863a3
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-10-17 07:17:02 +0530

    Instance Vs Class attributes

--- 6 Classes/4- Class vs Instance Attributes.py
+++ 6 Classes/4- Class vs Instance Attributes.py
@@ -1,22 +1,24 @@
-# 4- Class vs Instance Attributes
+# 4 Class vs instance Attributes
 
 class Point:
-	default_color = 'red'
+    default_color = 'red'
 
-    def __init__(self, x, y):	#constructor
-        self.x = x	#instance attribute
-        self.y = y 
+    def __init__(self,x,y):
+        self.x= x
+        self.y = y
 
     def draw(self):
-    	print('Drawing..')
-    	print(f'Point ({self.x},{self.y})')
+        print('Drawing...')
+        print(f'Point ({self.x},{self.y})')
 
-# first instance
+# creating instances
+Point.default_color = 'yellow'
+
 p1 = Point(1,2)
-print(p1.x)
-print(p1.y)
 p1.draw()
+print(Point.default_color)
+print(p1.default_color)
 
-# second instance
-p2 = Point(5,5)
+p2 = Point(5,10)
 p2.draw()
+print(p2.default_color)
